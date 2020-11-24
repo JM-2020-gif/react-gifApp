4eeca61c8254384dcf8f0dc3ad499018ec19d9a2
unknown <andreagutierrez160983@gmail.com>
2020-11-24 22:22:45 +0000

    add commit

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const GifGridItem = ({ title, url }) => {
 
+    //code below, allows you to render the cards
     return (
         <div className="card animate__animated animate__fadeIn">
             <img src={ url } alt={ title } />
