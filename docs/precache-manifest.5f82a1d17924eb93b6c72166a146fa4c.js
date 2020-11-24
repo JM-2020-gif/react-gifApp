8ee42a03595c395db2a42cbace83802a50402305
unknown <andreagutierrez160983@gmail.com>
2020-11-24 21:44:13 +0000

    add docs

---
+++ docs/precache-manifest.5f82a1d17924eb93b6c72166a146fa4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5834e29dd4d6adceecaa98e588cda752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9db19d9e9d6b42a21330",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db19d9e9d6b42a21330",
+    "url": "/static/js/main.664c7ca5.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
